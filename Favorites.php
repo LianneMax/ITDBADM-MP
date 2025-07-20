05fa51078db034f711b9247b3bc4fe1797951f68
pichioes <maria_alyssa_mansueto@dlsu.edu.ph>
2025-07-20 20:38:44 +0800

    updated db name in connection

--- Favorites.php
+++ Favorites.php
@@ -20,7 +20,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "techperipherals_db";
+$dbname = "pluggedin_itdbadm";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "pluggedin_store"; // Change if needed
+$dbname = "pluggedin_itdbadm";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
